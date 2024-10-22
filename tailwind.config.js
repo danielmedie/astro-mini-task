324fda3974052f7b93fecf64257a409d23a8e34d
danie <daniel.ek@skola.botkyrka.se>
2024-10-22 16:17:50 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Inkludera alla nödvändiga filtyper
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
